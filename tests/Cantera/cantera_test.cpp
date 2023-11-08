1b1cf6f9cbab537a9c5ff37346458a1b2da57cbd
Bogdan Danciu <danciu_bogdan94@yahoo.com>
2023-11-08 15:10:59 +0100

    Add line arguments to cantera test file

--- tests/Cantera/cantera_test.cpp
+++ tests/Cantera/cantera_test.cpp
@@ -1,12 +1,11 @@
 #include "mpi.h"
 #include <cmath>
-
 #include <cstring>
-#include <limits>
 #include <sstream>
 #include <string>
 #include <algorithm>
 #include <cstddef>
+#include <getopt.h>
 
 #include "cantera/core.h"
 #include "cantera/kinetics/Reaction.h"
@@ -17,30 +16,74 @@ using namespace Cantera;
 
 int main(int argc, char** argv)
 {
-  MPI_Init(NULL, NULL);
+
+  MPI_Init(&argc, &argv);
   int rank, size;
 
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-
   printf("size: %d\n", size);
-  int Nstates = 1000000/size;
-  constexpr int Nrep = 5;
 
-  int offset = 9;
-  
+  int err = 0;
+  int n_states = 100000;
+  int n_rep = 20;
+  std::string mech;
+
+  while(1) {
+    static struct option long_options[] =
+    {
+      {"n-states", required_argument, 0, 's'},
+      {"n-repetitions", required_argument, 0, 'r'},
+      {"mechanism", required_argument, 0, 'm'},
+    };
+
+    int option_index = 0;
+    int args = getopt_long(argc, argv, "s:", long_options, &option_index);
+
+    if (args == -1)
+      break;
+
+    switch(args) {
+    case 's':
+      n_states = std::stoi(optarg);
+      break;
+    case 'r':
+      n_rep = std::stoi(optarg);
+      break;
+    case 'm':
+      mech.assign(optarg);
+      break;
+
+    default:
+      err++;
+    }
+  }
+
+  if(mech.size() < 1) err++;
+
+  if(err > 0) {
+    if(rank == 0)
+      printf("Usage: ./cantera_test  [--n-states n] [--n-repetitions] [--mechanism f] \n");
+    exit(EXIT_FAILURE);
+  }
+
+  int Nstates = n_states/size;
+  int Nrep = n_rep;
+
   // Initialize reaction mechanism 
-  auto sol = newSolution("h2o2.yaml", "ohmech");
+  auto sol = newSolution(mech);
   auto gas = sol->thermo();
-  double temp = 1200.0;
+  double temp = 2800.0;
   double pres = OneAtm;
   gas->setState_TPX(temp, pres, "H2:1, O2:1, AR:2");
 
   // Reaction information
   auto kin = sol->kinetics();
+  int nSpecies = gas->nSpecies();
+  int offset = nSpecies + 1;
 
   // Initialize species production rates
-  std::vector<double> netProductionRates(gas->nSpecies(), 0.0);
+  std::vector<double> netProductionRates(nSpecies, 0.0);
 
   for(int i=0; i<Nrep; i++) {
 
@@ -48,7 +91,7 @@ int main(int argc, char** argv)
     const auto startTime = MPI_Wtime();
 
     for(int n=0; n<Nstates; n++) {
-      // Get the species net production rates
+      // Get species net production rates
       kin->getNetProductionRates(netProductionRates.data());      	
     }
 
@@ -64,4 +107,4 @@ int main(int argc, char** argv)
   exit(EXIT_SUCCESS);
 
 }
-                       
+
