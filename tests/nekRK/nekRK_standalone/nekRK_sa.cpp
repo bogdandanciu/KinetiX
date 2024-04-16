819ae002de25b091be89ad5ff678047626846d09
Bogdan Danciu <danciu_bogdan94@yahoo.com>
2024-04-16 17:35:48 +0200

    Update nekrk standalone

--- tests/nekRK/nekRK_standalone/nekRK_sa.cpp
+++ tests/nekRK/nekRK_standalone/nekRK_sa.cpp
@@ -18,9 +18,11 @@ int main(int argc, char **argv) {
   if (rank == 0)
     printf("size: %d\n", size);
 
-  int Nstates = 1000000 / size;
-  int Nrep = 20;
+  int Nstates = 1 / size;
+  int Nrep = 1;
+  bool transport = true;
   int offset = __NEKRK_NSPECIES__ + 1;
+
 
   // Initialize variables
   double T = 1000;
@@ -51,32 +53,24 @@ int main(int argc, char **argv) {
   double R;
   R = 1.380649e-23 * 6.02214076e23;
 
-  cfloat *ydot = (cfloat *)(_mm_malloc(Nstates * offset * sizeof(double), 64));
-  cfloat wdot[__NEKRK_NSPECIES__];
-  cfloat h_RT[__NEKRK_NSPECIES__];
-  cfloat sc[__NEKRK_NSPECIES__];
-
-
   /*** Throughput ***/
-  for(int ii=0; ii<4; ii++) {
-    if(ii==0) Nstates = 100/size;
-    if(ii==1) Nstates = 1000000/size;
-    if(ii==2) Nstates = 100000/size;
-    if(ii==3) Nstates = 10000/size;
-    Nstates = 1;
+  { // Chemistry
+    cfloat *ydot = (cfloat *)(_mm_malloc(Nstates * offset * sizeof(double), 64));
+    cfloat wdot[__NEKRK_NSPECIES__];
+    cfloat h_RT[__NEKRK_NSPECIES__];
+    cfloat sc[__NEKRK_NSPECIES__];
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    const auto startTime = MPI_Wtime();
 
     for (int i = 0; i < Nrep; i++) {
-  
-      MPI_Barrier(MPI_COMM_WORLD);
-      const auto startTime = MPI_Wtime();
-  
       for (int n = 0; n < Nstates; n++) {
         const cfloat rcpT = 1 / T;
         const cfloat logT = log(T);
         const cfloat T2 = T * T;
         const cfloat T3 = T * T * T;
         const cfloat T4 = T * T * T * T;
-  
+    
         cfloat W;
         cfloat wrk1[__NEKRK_NSPECIES__];
         {
@@ -93,7 +87,7 @@ int main(int argc, char **argv) {
         nekrk_species_rates(logT,T, T2, T3, T4, rcpT, sc, wdot);
         for (int k = 0; k < __NEKRK_NSPECIES__; k++)
           ydot[n + k * Nstates] = wdot[k] * nekrk_molar_mass[k];
-  
+    
         nekrk_enthalpy_RT(logT, T, T2, T3, T4, rcpT, h_RT);
         cfloat sum_h_RT = 0;
         for (int k = 0; k < __NEKRK_NSPECIES__; k++)
@@ -101,22 +95,78 @@ int main(int argc, char **argv) {
         cfloat ratesFactorEnergy = -R * T;
         ydot[n] = ratesFactorEnergy * sum_h_RT;
       }
-  
-      MPI_Barrier(MPI_COMM_WORLD);
-      const auto elapsedTime = (MPI_Wtime() - startTime);
-
-      if(rank == 0 && ii)
-        printf("avg aggregated throughput: %.3f GDOF/s (Nstates = %d)\n",
-               (size * (double)(Nstates * offset)) / elapsedTime / 1e9,
-               size * Nstates);
     }
-  } 
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+    const auto elapsedTime = (MPI_Wtime() - startTime);
+
+    if(rank == 0)
+      printf("--- Chemistry ---\n");
+      printf("avg aggregated throughput: %.3f GDOF/s (Nstates = %d)\n",
+             (size * (double)(Nstates * (__NEKRK_NSPECIES__ + 1) * Nrep)) / elapsedTime / 1e9,
+             size * Nstates);
 
 #if 0
-  for (int i=0; i< Nstates * offset; i++)
-    printf("rates[%d]: %.8f \n", i, ydot[i]);
+    for (int i=0; i< Nstates * (__NEKRK_NSPECIES__ + 1); i++)
+      printf("rates[%d]: %.8f \n", i, ydot[i]);
 #endif
+
+  } 
+
+  if (transport){ //Transport 
+    cfloat *cond = (cfloat *)(_mm_malloc(Nstates * sizeof(double), 64));
+    cfloat *visc = (cfloat *)(_mm_malloc(Nstates * sizeof(double), 64));
+    cfloat *rhoD = (cfloat *)(_mm_malloc(Nstates * __NEKRK_NSPECIES__ * sizeof(double), 64));
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    const auto startTime = MPI_Wtime();
+
+    for(int i=0; i<Nrep; i++) {
+      for(int n=0; n<Nstates; n++) {
+        const cfloat lnT = log(T);
+        const cfloat lnT2 = lnT * lnT;
+        const cfloat lnT3 = lnT * lnT * lnT;
+        const cfloat lnT4 = lnT * lnT * lnT * lnT;
+        const cfloat sqrT = sqrt(T);
+        
+	cfloat wrk1[__NEKRK_NSPECIES__];
+        
+	cfloat iW = 0;
+	{
+          for (int k = 0; k < __NEKRK_NSPECIES__; k++) {
+            wrk1[k] = Y[k] * nekrk_rcp_molar_mass[k];
+            iW += wrk1[k];
+          }
+	}
+
+        cond[n] = nekrk_conductivity(iW, lnT, lnT2, lnT3, lnT4, wrk1) * sqrT;
+        visc[n]  = nekrk_viscosity(lnT, lnT2, lnT3, lnT4, wrk1);
+        nekrk_density_diffusivity(n, sqrT, lnT, lnT2, lnT3, lnT4, wrk1, rhoD, Nstates);
+      }
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    const auto elapsedTime = (MPI_Wtime() - startTime);
+
+    if(rank == 0){
+      printf("--- Transport ---\n");
+      printf("avg aggregated throughput: %.3f GDOF/s (Nstates = %d)\n",
+             (size * (double)(Nstates*(__NEKRK_NSPECIES__ + 1) * Nrep))/ elapsedTime /1e9,
+             size*Nstates);
+    }
+
+#if 0
+    for (int i = 0; i < Nstates; i++){
+      printf("cond[%d]: %.8f \n", i, cond[i]);
+      printf("visc[%d]: %.8f \n", i, visc[i]);
+    }
+    for (int i = 0; i < Nstates * __NEKRK_NSPECIES__; i++)
+      printf("rhoD[%d]: %.8f \n", i, rhoD[i]);
+#endif
+ 
+  }
 
   MPI_Finalize();
   exit(EXIT_SUCCESS);
+
 }
