dd6ae524f0e763ac5a05de5fe9bf6a188b832878
Bogdan Danciu <danciu_bogdan94@yahoo.com>
2024-06-27 15:23:47 +0200

    Add mechanism files generated with Pele (CEPTR)

---
+++ mechanisms/Pele/LiDryer/mechanism.cpp
@@ -0,0 +1,177 @@
+#include "mechanism.H"
+const int rmap[21] = {8,  15, 4,  5,  6,  7,  0,  1,  2,  3, 9,
+                      10, 11, 12, 13, 14, 16, 17, 18, 19, 20};
+
+// Returns 0-based map of reaction order
+void
+GET_RMAP(int* _rmap)
+{
+  for (int j = 0; j < 21; ++j) {
+    _rmap[j] = rmap[j];
+  }
+}
+
+// Returns a count of species in a reaction, and their indices
+// and stoichiometric coefficients. (Eq 50)
+void
+CKINU(const int i, int& nspec, int ki[], int nu[])
+{
+  const int ns[21] = {4, 4, 4, 3, 2, 2, 3, 3, 3, 4, 3,
+                      4, 4, 3, 3, 2, 4, 4, 4, 4, 4};
+  const int kiv[84] = {3, 1, 4, 5, 0, 4, 3, 5, 0, 5, 3, 2, 2, 4, 5, 0, 0,
+                       3, 0, 0, 4, 1, 0, 0, 3, 4, 5, 0, 3, 5, 2, 0, 3, 1,
+                       6, 0, 3, 6, 0, 1, 3, 6, 5, 0, 6, 4, 1, 5, 6, 5, 2,
+                       1, 6, 7, 1, 0, 6, 7, 1, 0, 7, 5, 0, 0, 3, 7, 2, 5,
+                       3, 7, 0, 6, 7, 4, 6, 5, 7, 5, 2, 6, 7, 5, 2, 6};
+  const int nuv[84] = {-1, -1, 1,  1,  -1, -1, 1,  1,  -1, -1, 1,  1,  -1, -1,
+                       2,  0,  -1, 2,  0,  0,  -2, 1,  0,  0,  -1, -1, 1,  0,
+                       -1, -1, 1,  0,  -1, -1, 1,  0,  -1, -1, 1,  1,  -1, -1,
+                       2,  0,  -1, -1, 1,  1,  -1, -1, 1,  1,  -2, 1,  1,  0,
+                       -2, 1,  1,  0,  -1, 2,  0,  0,  -1, -1, 1,  1,  -1, -1,
+                       1,  1,  -1, -1, 1,  1,  -1, -1, 1,  1,  -1, -1, 1,  1};
+  if (i < 1) {
+    // Return max num species per reaction
+    nspec = 4;
+  } else {
+    if (i > 21) {
+      nspec = -1;
+    } else {
+      nspec = ns[i - 1];
+      for (int j = 0; j < nspec; ++j) {
+        ki[j] = kiv[(i - 1) * 4 + j] + 1;
+        nu[j] = nuv[(i - 1) * 4 + j];
+      }
+    }
+  }
+}
+
+// Returns the progress rates of each reactions
+// Given P, T, and mole fractions
+void
+CKKFKR(
+  const amrex::Real P,
+  const amrex::Real T,
+  const amrex::Real x[],
+  amrex::Real q_f[],
+  amrex::Real q_r[])
+{
+  amrex::Real c[9]; // temporary storage
+  amrex::Real PORT =
+    1e6 * P / (8.31446261815324e+07 * T); // 1e6 * P/RT so c goes to SI units
+
+  // Compute conversion, see Eq 10
+  for (int id = 0; id < 9; ++id) {
+    c[id] = x[id] * PORT;
+  }
+
+  // convert to chemkin units
+  progressRateFR(q_f, q_r, c, T);
+
+  // convert to chemkin units
+  for (int id = 0; id < 21; ++id) {
+    q_f[id] *= 1.0e-6;
+    q_r[id] *= 1.0e-6;
+  }
+}
+
+// compute the progress rate for each reaction
+// USES progressRate : todo switch to GPU
+void
+progressRateFR(
+  amrex::Real* q_f, amrex::Real* q_r, amrex::Real* sc, amrex::Real T)
+{
+  const amrex::Real invT = 1.0 / T;
+  const amrex::Real logT = log(T);
+  // compute the Gibbs free energy
+  amrex::Real g_RT[9];
+  gibbs(g_RT, T);
+
+  amrex::Real sc_qss[1];
+  comp_qfqr(q_f, q_r, sc, sc_qss, T, invT, logT);
+}
+
+// save atomic weights into array
+void
+atomicWeight(amrex::Real* awt)
+{
+  awt[0] = 1.008000;  // H
+  awt[1] = 15.999000; // O
+  awt[2] = 14.007000; // N
+}
+
+// get atomic weight for all elements
+void
+CKAWT(amrex::Real* awt)
+{
+  atomicWeight(awt);
+}
+
+// Returns the elemental composition
+// of the speciesi (mdim is num of elements)
+void
+CKNCF(int* ncf)
+{
+  int kd = 3;
+  // Zero ncf
+  for (int id = 0; id < kd * 9; ++id) {
+    ncf[id] = 0;
+  }
+
+  // H2
+  ncf[0 * kd + 0] = 2; // H
+
+  // O2
+  ncf[1 * kd + 1] = 2; // O
+
+  // H2O
+  ncf[2 * kd + 0] = 2; // H
+  ncf[2 * kd + 1] = 1; // O
+
+  // H
+  ncf[3 * kd + 0] = 1; // H
+
+  // O
+  ncf[4 * kd + 1] = 1; // O
+
+  // OH
+  ncf[5 * kd + 0] = 1; // H
+  ncf[5 * kd + 1] = 1; // O
+
+  // HO2
+  ncf[6 * kd + 0] = 1; // H
+  ncf[6 * kd + 1] = 2; // O
+
+  // H2O2
+  ncf[7 * kd + 0] = 2; // H
+  ncf[7 * kd + 1] = 2; // O
+
+  // N2
+  ncf[8 * kd + 2] = 2; // N
+}
+
+// Returns the vector of strings of element names
+void
+CKSYME_STR(amrex::Vector<std::string>& ename)
+{
+  ename.resize(3);
+  ename[0] = "H";
+  ename[1] = "O";
+  ename[2] = "N";
+}
+
+// Returns the vector of strings of species names
+void
+CKSYMS_STR(amrex::Vector<std::string>& kname)
+{
+  kname.resize(9);
+  kname[0] = "H2";
+  kname[1] = "O2";
+  kname[2] = "H2O";
+  kname[3] = "H";
+  kname[4] = "O";
+  kname[5] = "OH";
+  kname[6] = "HO2";
+  kname[7] = "H2O2";
+  kname[8] = "N2";
+}
+
